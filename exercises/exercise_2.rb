1fd2d0b8a7f529b706f93c832d854327eb7bd792
Young <mcpjjh@gmail.com>
2017-02-15 00:26:32 +0000

    update the first store to a different name

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,13 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+@store1 = Store.find(1)
+@store2 = Store.find(2)
+
+#or
+# s = Store.find(1)
+# s.name = "Coquitlam"
+# s.save
+
+@store1.update(name: "Coquitlam")
+
