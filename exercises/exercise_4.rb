6b721919f5d01ccfd3a536f9c0b44605a79d16f5
Young <mcpjjh@gmail.com>
2017-02-15 00:28:34 +0000

    add 3 more stores, assign @mens_stores then output their names and revenues, assign @womens_stores

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,14 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+
+
+Store.create!(name: "Surrey", annual_revenue: 224000, womens_apparel: true)
+Store.create!(name: "Whistler", annual_revenue: 1900000, mens_apparel: true)
+Store.create!(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+@mens_stores = Store.where(mens_apparel: true)
+
+@mens_stores.map {|store| puts store.name, store.annual_revenue}
+
+@womens_stores = Store.where(womens_apparel: true).where('annual_revenue < 1000000')
